129d58e9df2d93502b0c8da316054f84ff65673d
vmlakk <lodyginap@gmail.com>
2024-04-30 16:12:46 +0300

    add hangman stages

---
+++ src/HangmanStages.kt
@@ -0,0 +1,55 @@
+object HangmanStages {
+
+     fun getHangman(mistakes: Int): String {
+         return hangmanStages[mistakes]
+     }
+
+     fun isFail(mistakes: Int): Boolean {
+          return mistakes >= hangmanStages.size - 1
+     }
+
+     private val hangmanStages = listOf(
+          """
+         |
+         |
+         |
+         |
+         |
+     """,
+          """
+         |———|
+         |
+         |
+         |
+         |
+     """,
+          """
+         |———|
+         |   O
+         |
+         |
+         |
+     """,
+          """
+         |———|
+         |   O
+         |   |
+         |
+         |
+     """,
+          """
+         |———|
+         |   O
+         |  /|\
+         |
+         |
+     """,
+          """
+         |———|
+         |   O
+         |  /|\
+         |  / \
+         |
+     """
+     ).map { it.trimIndent() }
+}
